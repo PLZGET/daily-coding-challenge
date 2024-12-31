32a2c6b70d856bf81c5c6619ea7f8fd15c70c424
이정훈 <niko36@naver.com>
2025-01-01 03:11:10 +0900

    Solve: <graph BFS basic code> 2025_01_01

---
+++ 기본문제모음/해시테이블/bfs.py
@@ -0,0 +1,24 @@
+# 그래프 bfs 기본 코드
+from collections import deque
+
+def bfs(graph, start_v):
+    visited = []  # 방문한 노드 리스트
+    queue = deque([start_v])  # 큐를 올바르게 초기화
+    
+    while queue:
+        cur_v = queue.popleft()
+        for v in graph[cur_v]:
+            if v not in visited:
+                visited.append(v)
+                queue.append(v)
+    return visited
+
+graph = {
+    'A' : ['B', 'D', 'E'],
+    'B' : ['A', 'C', 'D'],
+    'C' : ['B'],
+    'D' : ['A', 'B'],
+    'E' : ['A']
+}
+
+print(bfs(graph, 'A'))
